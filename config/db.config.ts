51abe09e66064e16ac34de42f884ac977a78a759
Clidenor Issac <issaccabrall@gmail.com>
2022-02-25 09:12:08 -0300

    Application Setup

---
+++ config/db.config.ts
@@ -0,0 +1,38 @@
+/**Mudamos de config.json para db.config.ts
+ * pois assim podemos utilizar as variáveis de
+ * ambiente dentro desse arquivo e dar uma camada 
+ * se segurança
+ */
+import env from 'dotenv'
+env.config()
+
+export default
+{
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "logging": true
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "logging": true
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "logging": false
+  }
+}
--- readme.md
+++
@@ -1,7 +0,0 @@
-<h2>Blog_api_REST_Typescript</h2>
-
-* Migrations
-* Autenticação com JWT
-* Validação de dados
-* Middlewares
-* dotenv
